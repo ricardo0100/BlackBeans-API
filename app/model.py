0d11cc3c9d04bccc7c7668c07378e8a6d85a37cf
Ricardo Gehrke Filho <ricardo.gehrke@arctouch.com>
2020-05-17 09:07:42 -0300

    Add isActive field in Account

--- app/model.py
+++ app/model.py
@@ -5,15 +5,17 @@ class Account(db.Model):
     __tablename__ = "accounts"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
-    creation = db.Column(db.Integer)
-    update = db.Column(db.Integer)
+    creation = db.Column(db.Float)
+    update = db.Column(db.Float)
+    isActive = db.Column(db.Boolean)
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
             "creation": self.creation,
-            "update": self.update
+            "update": self.update,
+            "isActive": self.isActive
         }
 
 
--- app/views.py
+++ app/views.py
@@ -2,18 +2,21 @@ from flask import jsonify, request
 from flask_restful import Resource
 from app import app, api, db
 from app.model import Account, Category
+import time
 
 
 @app.route("/")
 def home():
-    return "BlackBeans 🐶<br><a href='https://github.com/ricardo0100/BlackBeansAPI'>Github page</a>"
+    return "BlackBeans 🐶<br/><a href='https://github.com/ricardo0100/BlackBeansAPI'>Github page</a>"
 
 
 # Accounts
 class AccountsListResource(Resource):
     @staticmethod
     def get():
-        return jsonify(list(map(lambda account: account.serialize(), Account.query.all())))
+        after_timestamp = request.args.get('updated_after')
+        accounts = Account.query.filter(Account.update > after_timestamp).all()
+        return jsonify(list(map(lambda account: account.serialize(), accounts)))
 
     @staticmethod
     def post():
@@ -22,6 +25,7 @@ class AccountsListResource(Resource):
         account.name = json["name"]
         account.update = json["update"]
         account.creation = json["creation"]
+        account.isActive = True
         db.session.add(account)
         db.session.commit()
         return account.serialize()
@@ -38,6 +42,7 @@ class AccountsResource(Resource):
         json = request.json
         account.name = json["name"]
         account.update = json["update"]
+        account.isActive = json["isActive"]
         db.session.commit()
         return account.serialize()
 
