285f82021f212a4cbce5c2d05335f4abc7a6485b
Ricardo Gehrke Filho <ricardo.filho@luizalabs.com>
2022-05-13 10:36:59 -0300

    Add new svelte front end

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='/assets/', 
+            static_folder='templates/assets/',
+            template_folder='templates/')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('MYSQL_URI', 'mysql://root@localhost:3306/black_beans')
 
-CORS(app)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("MYSQL_URI", "mysql://root@localhost:3306/black_beans")
 db = SQLAlchemy(app)
 api = Api(app)
 
